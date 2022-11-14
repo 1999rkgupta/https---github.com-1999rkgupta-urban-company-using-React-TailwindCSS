61d2bf730651b233e67b1d642ccaafc152321983
1999rkgupta <1999rkgupta@gmail.com>
2022-11-14 13:45:22 +0530

    updated

---
+++ src/ComponentsComposition/Pages.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Header/Navbar";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+
+const Pages = () => {
+return (
+  <div id="navBlock">
+    <Navbar />
+    <Main />
+    <Footer />
+  </div>
+);
+};
+
+export default Pages;
